a2ab548c2196e86989689a3a9aabe70e63577203
hao.h <hoanghao160894@gmail.com>
2023-06-28 06:49:55 +0700

    vnPay

---
+++ src/main/java/com/mycompany/myapp/vnPay/PaymentInforDTO.java
@@ -0,0 +1,54 @@
+package com.mycompany.myapp.vnPay;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PaymentInforDTO {
+    private String vnp_Amount;
+    private String vnp_BankCode;
+    private String vnp_OrderInfo;
+    private String vnp_ResponseCode;
+    private String vnp_PayDate;
+
+    public String getVnp_Amount() {
+        return vnp_Amount;
+    }
+
+    public void setVnp_Amount(String vnp_Amount) {
+        this.vnp_Amount = vnp_Amount;
+    }
+
+    public String getVnp_BankCode() {
+        return vnp_BankCode;
+    }
+
+    public void setVnp_BankCode(String vnp_BankCode) {
+        this.vnp_BankCode = vnp_BankCode;
+    }
+
+    public String getVnp_OrderInfo() {
+        return vnp_OrderInfo;
+    }
+
+    public void setVnp_OrderInfo(String vnp_OrderInfo) {
+        this.vnp_OrderInfo = vnp_OrderInfo;
+    }
+
+    public String getVnp_ResponseCode() {
+        return vnp_ResponseCode;
+    }
+
+    public void setVnp_ResponseCode(String vnp_ResponseCode) {
+        this.vnp_ResponseCode = vnp_ResponseCode;
+    }
+
+    public String getVnp_PayDate() {
+        return vnp_PayDate;
+    }
+
+    public void setVnp_PayDate(String vnp_PayDate) {
+        this.vnp_PayDate = vnp_PayDate;
+    }
+}
